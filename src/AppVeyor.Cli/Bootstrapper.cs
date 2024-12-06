a3511670bea1a721bf87ae7442eb93ef2634e299
Mohamed Hassan <moh.hassan20@gmail.com>
2024-12-07 00:45:04 +0200

    Refactor Main method.

---
+++ src/AppVeyor.Cli/Bootstrapper.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Mohamed Hassan. All rights reserved. See License.md in the project root for license information.
+
+namespace AppVeyor.Cli;
+
+using Api.Utility;
+using Commands;
+using Cli = DotMake.CommandLine.Cli;
+
+public class Bootstrapper
+{
+    public ExecutionInfo ExecutionInfo { get; set; } = new();
+
+    public async Task<int> StartAsync(string[] args)
+    {
+        var cancellationTokenSource = new CancellationTokenSource();
+        // Hook into Ctrl+C event
+        Console.CancelKeyPress += (s, a) =>
+        {
+            a.Cancel = true; // Prevent default termination
+            WriteWarning("Application is stopped by user.");
+            cancellationTokenSource.Cancel(); // Cancel the token
+        };
+
+        var setting = new CliSettings
+        {
+            EnableDiagramDirective = true,
+            ProcessTerminationTimeout = TimeSpan.FromSeconds(5.0),
+            EnableEnvironmentVariablesDirective = true,
+            Theme = CliTheme.Green,
+        };
+        string[] helpArgs = { "-h", "-?", "--help", "--version" };
+
+        var pi = new AppVersionInfo(typeof(Program).Assembly);
+        var heading = pi.Heading;
+        WriteInfo(heading);
+        var exitCode = await Cli.RunAsync<AppveyorCommand>(args, setting, cancellationTokenSource.Token);
+
+        if (exitCode == 0)
+        {
+            if (!(args.Length == 0 || helpArgs.Any(args.Contains)))
+                WriteSuccess("Exit code 0");
+        }
+        else
+        {
+            WriteVitalError($"Exit code: {exitCode}");
+        }
+
+        return exitCode;
+    }
+}
--- src/AppVeyor.Cli/Program.cs
+++ src/AppVeyor.Cli/Program.cs
@@ -7,10 +7,7 @@ using System.Runtime.InteropServices;
 
 namespace AppVeyor.Cli;
 
-using System.Threading;
-using Api.Utility;
 using Commands;
-using DotMake.CommandLine;
 
 public static class Program
 {
@@ -18,56 +15,28 @@ public static class Program
 
     public static async Task<int> Main(string[] args)
     {
-        var cancellationTokenSource = new CancellationTokenSource();
-        // Hook into Ctrl+C event
-        Console.CancelKeyPress += (s, a) =>
-        {
-            a.Cancel = true; // Prevent default termination
-            WriteWarning("Application is stopped by user.");
-            cancellationTokenSource.Cancel(); // Cancel the token
-        };
-
-        var setting = new CliSettings
-        {
-            EnableDiagramDirective = true,
-            ProcessTerminationTimeout = TimeSpan.FromSeconds(5.0),
-            EnableEnvironmentVariablesDirective = true,
-            Theme = CliTheme.Green,
-        };
-        string[] helpArgs = { "-h", "-?", "--help", "--version" };
         try
         {
-            var pi = new AppVersionInfo(typeof(Program).Assembly);
-            var heading = pi.Heading;
-            WriteInfo(heading);
-            var exitCode = await Cli.RunAsync<AppveyorCommand>(args, setting, cancellationTokenSource.Token);
-
-            if (exitCode == 0)
-            {
-                if (!(args.Length == 0 || helpArgs.Any(args.Contains)))
-                    WriteSuccess("Exit code 0");
-            }
-            else
-            {
-                WriteVitalError($"Exit code: {exitCode}");
-            }
-
-            return exitCode;
+            var bootstrapper = new Bootstrapper();
+            ExecutionInfo = bootstrapper.ExecutionInfo;
+            return await bootstrapper.StartAsync(args);
         }
         catch (OperationCanceledException)
         {
             //do nothing
+            Console.WriteLine("Operation is cancelled by user");
+            return 0;
         }
         catch (Exception e)
         {
             WriteError($"Error: {e.Message}\n{e.InnerException?.Message}");
             WriteVitalError($"Exit code: 3");
+
 #if DEBUG
             Console.WriteLine("---------------- Debug StackTrace------------------");
             Console.WriteLine(e.StackTrace);
 #endif
+            return 3;
         }
-
-        return 1;
     }
 }
